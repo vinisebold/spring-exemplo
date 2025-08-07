46429c91417a620b34ff13ce7952b0db09fa8d77
vinisebold <vinicius.sebold05@gmail.com>
2025-08-07 16:59:01 -0300

    feat: implementa endpoint de delete

--- src/main/resources/static/js/deletar.js
+++ src/main/resources/static/js/deletar.js
@@ -1,13 +1,11 @@
-document.getElementById('formulario').addEventListener('submit', function(e){
+document.getElementById("formulario").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const produto = {
-    id: parseInt(document.getElementById('id').value),
-  }
+  const id = parseInt(document.getElementById("id").value);
 
-  fetch('http://localhost:8080/produtos', {
-      method: 'DELETE',
-      headers: {'Content-Type' : 'application/json'},
-      body: JSON.stringify(produto)
-  })
-})
+  fetch(`http://localhost:8080/produtos/${id}`, {
+    method: "DELETE",
+  });
+
+  document.getElementById("formulario").reset();
+});
